7b45fbc6a03ab35417e9c82552d6e592d143f288
David Riggins <driggins@esri.com>
2025-08-06 10:48:56 -0700

    End Lesson 15

---
+++ ButtonDemo/ButtonDemo/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            double myFontSize = myLabel.FontSize;
+            myLabel.Foreground = Brushes.Coral;
+            myLabel.FontSize = myFontSize + 1;
+        }
+
+        private void myButton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            double myFontSize = myLabel.FontSize;
+            myLabel.Foreground = Brushes.Coral;
+            myLabel.FontSize = myFontSize -1;
+        }
+
+        private void myButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            myLabel.Foreground = Brushes.White;
+        }
+
+        private void myButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            myLabel.Foreground= Brushes.Black;
+        }
+    }
+}
