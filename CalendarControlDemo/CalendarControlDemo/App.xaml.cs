89ffefa07c56fee305c359b37f180eacd541514f
David Riggins <driggins@esri.com>
2025-08-07 12:16:54 -0700

    End Lesson 21

---
+++ CalendarControlDemo/CalendarControlDemo/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CalendarControlDemo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ CalendarControlDemo/CalendarControlDemo/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalendarControlDemo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        myTextBlock.Text = myCalendar.SelectedDate?.ToString("D") ?? "No date selected";
+    }
+
+    private void myCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+    {
+        myTextBlock.Text = myCalendar.SelectedDate?.ToString("D") ?? "No date selected";
+    }
+}
