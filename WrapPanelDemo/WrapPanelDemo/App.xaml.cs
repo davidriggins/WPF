753b2e60266fce344919343c95175723197c6ec3
David Riggins <driggins@esri.com>
2025-08-08 09:52:13 -0700

    End Lesson 28

---
+++ WrapPanelDemo/WrapPanelDemo/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WrapPanelDemo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
