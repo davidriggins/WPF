df93348c9e9a1ff2a8dbe5a13a42095c798559f6
David Riggins <driggins@esri.com>
2025-08-12 14:18:58 -0700

    End Lesson 44

---
+++ DataBindingComboBoxEnumDemo/DataBindingComboBoxEnumDemo/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingComboBoxEnumDemo
+{
+    public enum Animal
+    {
+        Dog,
+        Cat,
+        Bird,
+        Fish,
+        Reptile,
+        Hamster,
+        Rabbit,
+        Turtle,
+        GuineaPig,
+        Ferret
+    }
+}
