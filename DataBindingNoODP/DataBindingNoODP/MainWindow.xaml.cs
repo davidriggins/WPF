96a9b46b04177fd3ee749d47a8ffd323b8347b99
David Riggins <driggins@esri.com>
2025-08-12 12:56:02 -0700

    End Lesson 43

---
+++ DataBindingNoODP/DataBindingNoODP/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingNoODP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<int> availableNumbers { get; set; }
+
+        public MainWindow()
+        {
+            availableNumbers = new ObservableCollection<int>();
+            int counter = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                availableNumbers.Add(counter++);
+            }
+
+            InitializeComponent();
+            
+
+            // This line in XAML
+            // DataContext="{Binding RelativeSource={RelativeSource Self}}"
+            // Does the same as this one:
+            // this.DataContext = this;
+        }
+
+        private void AddNumber(object sender, RoutedEventArgs e)
+        {
+            availableNumbers.Add(1);
+        }
+
+        private void DeleteNumber(object sender, RoutedEventArgs e)
+        {
+            availableNumbers.RemoveAt(0);
+        }
+    }
+}
