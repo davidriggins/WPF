6a75e4f48542957c93b3a2c08fd9f1770668087d
David Riggins <driggins@esri.com>
2025-08-13 11:05:34 -0700

    End Lesson 51

---
+++ SimpleNavigationDemo/SimpleNavigationDemo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using SimpleNavigationDemo.Pages;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleNavigationDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Page1 FirstPage;
+        public Page2 SecondPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FirstPage = new Page1();
+            SecondPage = new Page2();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = FirstPage;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = SecondPage;
+        }
+    }
+}
