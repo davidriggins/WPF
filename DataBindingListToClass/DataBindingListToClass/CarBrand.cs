bb8ced0090dc9359f5d21bca175fb994ed62123e
David Riggins <driggins@esri.com>
2025-08-13 10:06:55 -0700

    End Lesson 45

---
+++ DataBindingListToClass/DataBindingListToClass/CarBrand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingListToClass
+{
+    public enum CarBrand
+    {
+        Toyota,
+        Ford,
+        BMW,
+        Honda,
+        Mercedes,
+        Volkswagen,
+        Audi,
+        Hyundai,
+        Nissan,
+        Chevrolet
+    }
+}
---
+++ DataBindingListToClass/DataBindingListToClass/Cars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingListToClass
+{
+    public class Cars
+    {
+        public static List<Car> GetCars()
+        {
+            return new List<Car>
+            {
+                new Car { Owner = "Alice", Type = CarType.Sedan, Brand = CarBrand.Audi },
+                new Car { Owner = "Bob", Type = CarType.SUV, Brand = CarBrand.Toyota },
+                new Car { Owner = "Charlie", Type = CarType.Hatchback, Brand = CarBrand.Ford },
+                new Car { Owner = "David", Type = CarType.Sedan, Brand = CarBrand.BMW },
+                new Car { Owner = "Eve", Type = CarType.SUV, Brand = CarBrand.Honda },
+            };
+        }
+    }
+}
