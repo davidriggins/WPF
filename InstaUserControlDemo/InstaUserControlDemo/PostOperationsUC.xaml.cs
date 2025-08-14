e2a479f0efcc44a6bd41bab17862b49a8b70c149
David Riggins <driggins@esri.com>
2025-08-14 14:06:57 -0700

    End Lesson 65

---
+++ InstaUserControlDemo/InstaUserControlDemo/PostOperationsUC.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InstaUserControlDemo
+{
+    /// <summary>
+    /// Interaction logic for PostOperationsUC.xaml
+    /// </summary>
+    public partial class PostOperationsUC : UserControl
+    {
+        public bool PostLiked { get; set; }
+
+        public PostOperationsUC()
+        {
+            InitializeComponent();
+        }
+
+        public void LikePost()
+        {
+            Heart.Source = new BitmapImage(
+                new Uri("pack://application:,,,/Icons/like.png", UriKind.Absolute));
+
+            PostLiked = true;
+        }
+
+        public void UnikePost()
+        {
+            Heart.Source = new BitmapImage(
+                new Uri("pack://application:,,,/Icons/nolike.png", UriKind.Absolute));
+
+            PostLiked = false;
+        }
+
+        private void Heart_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!PostLiked)
+            {
+                LikePost();
+            }
+            else
+            {
+                UnikePost();
+            }
+        }
+    }
+}
