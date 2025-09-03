25047d01cfde32645668629e119d63dab8b49636
David Riggins <driggins@esri.com>
2025-09-03 11:01:06 -0700

    104 - Implementing MahApps

--- WindowsStoreClone/WindowsStoreClone/MainWindow.xaml.cs
+++ WindowsStoreClone/WindowsStoreClone/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using MahApps.Metro.Controls;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,7 +16,7 @@ namespace WindowsStoreClone;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : MetroWindow
 {
     private Main MainWindowContentPage;
     private TopAppsWrapped MyTopAppsWrappedPage;
